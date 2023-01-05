681dd8b957109993b084db33760e30935b586975
Argon <argon@nogra.xyz>
2023-01-05 22:27:37 +0800

    sussy

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def video():
 
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    #from waitress import serve
+    app.run(host="0.0.0.0", port=8080)
