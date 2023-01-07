946634e3f476479cda4b2752363b2ac0f86968c8
Argon <argon@nogra.xyz>
2023-01-07 09:50:51 +0800

    add status endpoint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, redirect, render_template, request, abort
+import time
+
+from flask import Flask, redirect, render_template, request, abort, jsonify
 import os
 
 app = Flask(__name__)
@@ -25,7 +27,32 @@ def video():
     else:
         abort(400, "Missing url parameter")
 
+@app.route('/')
+def index():
+    redirect("https://discord.com/oauth2/authorize?client_id=844489130822074390&permissions=313344&scope=bot")
+
+@app.route('/status')
+def get_status():
+    current_time = time.time()
+    start_time = app.config['START_TIME']
+
+    # Calculate the uptime in seconds, minutes, and hours
+    uptime_seconds = int(current_time - start_time)
+    uptime_minutes, uptime_seconds = divmod(uptime_seconds, 60)
+    uptime_hours, uptime_minutes = divmod(uptime_minutes, 60)
+
+    # Create the JSON object with the status information
+    status = {
+        "status": "OK",
+        "uptime": f"{uptime_hours} hours, {uptime_minutes} minutes, {uptime_seconds} seconds",
+        "last_restart": app.config['START_TIME']
+    }
+
+    # Return the JSON object as the response
+    return jsonify(status)
+
 
 if __name__ == '__main__':
-    #from waitress import serve
+    # from waitress import serve
+    app.config['START_TIME'] = time.time()
     app.run(host="0.0.0.0", port=8080)
